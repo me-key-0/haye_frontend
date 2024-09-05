c718148013ad9950ab4c135dafd256343f4794ff
me-key-0 <121063294+me-key-0@users.noreply.github.com>
2024-09-05 12:01:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import HomePage from "./pages/HomePage";
-import Explore from "./pages/Explore.page";
-import Events from "./pages/Events.page";
-import Places from "./pages/Places.page";
-import PlaceDetailsPage from "./pages/PlaceDetails.page";
-import SignIn from "./pages/SignIn.page";
-import SignUp from "./pages/SignUp.page";
-import OtpPage from "./pages/OTP.page";
-import Header from "./components/Header.component";
-import ContactUs from "./pages/ContactUs.page";
+
+import HomePage from './pages/HomePage';
+import Explore from './pages/Explore.page';
+import Events from './pages/Events.page';
+import Places from './pages/Places.page';
+import PlaceDetailsPage from './pages/PlaceDetails.page'; 
+import SignIn from './pages/SignIn.page';
+import SignUp from './pages/SignUp.page';
+import OtpPage from './pages/OTP.page';
+import Header from './components/Header.component';
+import ContactUs from './pages/ContactUs.page';
 
 const App = ({ currentUser }) => {
   return (
@@ -28,12 +24,12 @@ const App = ({ currentUser }) => {
         <Route path="/events" element={<Events />} />
         <Route path="/places" element={<Places />} />
         <Route path="/places/:id" element={<PlaceDetailsPage />} />
-        <Route
-          path="/signin"
-          element={currentUser ? <Navigate to="/" /> : <SignIn />}
+        <Route 
+          path="/signin" 
+          element={currentUser ? <Navigate to="/" /> : <SignIn />} 
         />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/signup/otp" element={<OtpPage />} />
+        <Route path="/otp" element={<OtpPage />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
