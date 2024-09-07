83a19c831b3ae9e4b7cb212a800b47acce8f8217
me-key-0 <me.key.as.24@gmail.com>
2024-09-07 10:52:24 +0300

    Update

--- src/services/api/axiosInstance.js
+++ src/services/api/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-
-  baseURL: "https://haye-backend.onrender.com/",
+  baseURL: "http://localhost:3030",
 
   headers: {
     "Content-Type": "application/json",
